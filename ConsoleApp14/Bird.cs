5ae92930d79f6fd71a468efdcc50bc5d05819d25
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-06-21 12:46:56 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Bird
+    {
+        public List<int> soni =new List<int> {1, 2, 3, 4, 5 };
+
+        public Bird()
+        {
+            foreach (int i in soni)
+            {
+                Console.WriteLine( i);
+            }
+        }
+
+    }
+    
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    public class BirdCount
+    {
+        public static int[] LastWeek()
+        {
+            return new int[] { 0, 2, 5, 3, 7, 8, 4 };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lastWeekCounts = BirdCount.LastWeek();
+
+            Console.Write("O‘tgan haftadagi qushlar soni: ");
+            foreach (int count in lastWeekCounts)
+            {
+                Console.Write(count+", ");
+            }
+        }
+    }
+}
